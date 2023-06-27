76b8ea2a3b4b336dc4b2cecbbc6c4e61ff9cf772
Marina <PyatkovaM@yandex.ru>
2023-06-27 16:37:26 +0300

    решение 27 задачи

---
+++ 27/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 1. Input
+int A = GetNumberFromUser("Введите целое число A", "Ошибка ввода");
+
+// 2. Calculation
+int result = SumNumber(A);
+
+// 3. Output
+Console.WriteLine($"сумма цифр в числе --> {result}");
+
+////////////////////////////////////////////////////////
+
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        if(int.TryParse(Console.ReadLine(), out int UserNumber))
+            return UserNumber;
+
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int SumNumber(int number)
+{
+    int sum = 0;
+    while (A > 0)
+    {
+        sum = sum + A % 10;
+        A = A /10 ;
+    }
+
+    return sum;
+}
