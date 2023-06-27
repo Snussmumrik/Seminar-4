0e83fa77f53966d92c75bc483b1d80a60331637b
Marina <PyatkovaM@yandex.ru>
2023-06-27 16:37:56 +0300

    решиен 29 задачи

---
+++ 29/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+Console.Clear();
+
+int[]array = GetArray(8, 1, 100);
+Console.WriteLine (string.Join (" ", array));
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue,maxValue+1);
+    }
+    return res;
+}
